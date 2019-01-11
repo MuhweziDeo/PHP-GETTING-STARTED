c341ed4d09e7e2d6f0c3308400571fcf2382a5ac
muhwezi deo <aggrey256@gmail.com>
2019-01-11 23:19:30 +0300

    init repo

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo('hello DEE');
+    echo '<hr>';
+    echo "<h1>dee<h1/>";
+    echo 'there was once named a man john <br>';
+    echo 'he was 35 years old <br>';
+    echo 'really liked the name john <br>';
+    echo 'but he didnt like being 35 <br>';
+    
+    ?>
+</body>
+</html>
