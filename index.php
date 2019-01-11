b0bf47b87c95e837e06081c516105a1f458d7d08
muhwezi deo <aggrey256@gmail.com>
2019-01-11 23:32:22 +0300

    test variables

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>PHP</title>
 </head>
+
 <body>
+
+
+
     <?php
-    echo('hello DEE');
-    echo '<hr>';
+    // variables
+    $characterName='John';
+    $characterAge=45;
+
+  
     echo "<h1>dee<h1/>";
-    echo 'there was once named a man john <br>';
-    echo 'he was 35 years old <br>';
-    echo 'really liked the name john <br>';
-    echo 'but he didnt like being 35 <br>';
+    echo "there was once named a man $characterName <br>";
+    echo "he was $characterAge years old <br>";
+    $characterName='dee';
+    echo "really liked the name $characterName <br>";
+    echo "but he didnt like being $characterAge <br>";
     
     ?>
 </body>
+
 </html>
